f5967121fe284c9973813d70324358ce1b4ad3ee
luansilvas <luanssantos12@gmail.com>
2023-02-01 00:06:30 -0300

    final adjusts for jumping on the clouds challenge from hackerrank. Check it at https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem

--- src/main/kotlin/challenges/JumpingClouds.kt
+++ src/main/kotlin/challenges/JumpingClouds.kt
@@ -1,29 +1,31 @@
 package challenges
 
+/**
+ * @see <a href="https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem">Jumping on the Clouds</a>
+ */
 fun jumpingOnClouds(c: Array<Int>): Int {
     // Write your code here
-    var index = 0
-    var jumps = 0
+    var currentIndex = 0
+    var givenJumps = 0
 
 
-    while (index < c.size) {
-        if (index < c.size - 2) {
-            if (c[index + 2] == 0) {
-                println("pulando dois hein")
-                index += 2
-                jumps +=1
-            } else {
-                println("pulando um hein")
-                index += 1
-                jumps +=1
+    while (currentIndex < c.size) {
+        if (currentIndex < c.size - 2) {
+            if (c[currentIndex + 2] == 0) {
+                currentIndex += 2
+                givenJumps += 1
+            } else { //will find thunderheads if we jump 2 clouds
+                currentIndex += 1
+                givenJumps += 1
             }
 
         } else {
-            index += 1
+            if (currentIndex == c.size - 2) { //lets suppose that it doesnt jump two clouds, there will be one missing thats it
+                givenJumps += 1
+            }
+            currentIndex += 1
         }
-
-
     }
 
-    return jumps
+    return givenJumps
 }
--- src/test/kotlin/challenges/JumpingCloudsKtTest.kt
+++ src/test/kotlin/challenges/JumpingCloudsKtTest.kt
@@ -6,10 +6,19 @@ import org.junit.jupiter.api.Assertions.*
 class JumpingCloudsKtTest{
 
     @Test
-    fun `should calculate the path that the least clouds to get to end successfully`(){
+    fun `case 0 - should calculate the path that the least clouds to get to end successfully`(){
 
         val expected = 4
         val result = jumpingOnClouds(arrayOf(0, 0, 1, 0, 0, 1, 0))
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `case 1 - should calculate the path that the least clouds to get to end successfully`(){
+
+        val expected = 3
+        val result = jumpingOnClouds(arrayOf(0, 0, 0, 1, 0,0))
 
         assertEquals(expected, result)
     }
